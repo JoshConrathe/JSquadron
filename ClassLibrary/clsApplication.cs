d03c3f0d0c8178db23196f3f6c2968a752d42e54
Przemek Waliszka <P2729217@LEC-ADMIN.dmu.ac.uk>
2024-05-13 14:18:18 +0100

    Add class and further unit tests

--- ClassLibrary/clsApplication.cs
+++ ClassLibrary/clsApplication.cs
@@ -3,14 +3,14 @@
     public class clsApplication
     {
         // Fields
-        public int ApplicationId { get; private set; }
+        public int ApplicationId { get; set; }
         public int? AdminId { get; set; }
         public int? StaffId { get; set; }
-        public string ApplicantName { get; private set; }
-        public string ContactNumber { get; private set; }
-        public string EmailAddress { get; private set; }
-        public string PoistionApplied { get; private set; }
-        public string Resume { get; private set; }
+        public string ApplicantName { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string PoistionApplied { get; set; }
+        public string Resume { get; set; }
 
         // Constructor
 
--- Testing4/tstApplication.cs
+++ Testing4/tstApplication.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net.Mail;
 
 namespace Testing4
 {
@@ -12,6 +13,89 @@ namespace Testing4
         {
             clsApplication jobApplication = new clsApplication();
             Assert.IsNotNull(jobApplication);
+        }
+
+        [TestMethod]
+        public void instanceConstructorWithPropertiesOk()
+        {
+            String applicantName = "TestName";
+            String emailAddress = "testEmail";
+            String contactNumber = "test0762318";
+            String poistionApplied = "testPosition";
+
+            clsApplication jobApplication = new clsApplication(applicantName, emailAddress, contactNumber, poistionApplied);
+
+            Assert.IsNotNull(jobApplication);
+        }
+        [TestMethod]
+        public void appIdPropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            int TestId = 643;
+
+            jobApp.ApplicationId = TestId;
+            Assert.AreEqual(jobApp.ApplicationId, TestId);
+        }
+
+        [TestMethod]
+        public void adminIdPropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            int TestId = 661;
+
+            jobApp.AdminId = TestId;
+            Assert.AreEqual(jobApp.AdminId, TestId);
+        }
+
+        [TestMethod]
+        public void staffIdPropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            int TestId = 3123;
+
+            jobApp.StaffId = TestId;
+            Assert.AreEqual(jobApp.StaffId, TestId);
+        }
+
+
+        [TestMethod]
+        public void namePropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            String TestName = "Przemek";
+
+            jobApp.ApplicantName = TestName;
+            Assert.AreEqual(jobApp.ApplicantName, TestName);
+        }
+
+        [TestMethod]
+        public void emailPropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            String TestEmail = "Przemek@gmail.com";
+
+            jobApp.EmailAddress = TestEmail;
+            Assert.AreEqual(jobApp.EmailAddress, TestEmail);
+        }
+
+        [TestMethod]
+        public void contactPropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            String TestContact = "076534192";
+
+            jobApp.ContactNumber = TestContact;
+            Assert.AreEqual(jobApp.ContactNumber, TestContact);
+        }
+
+        [TestMethod]
+        public void positionPropertyMethodOk()
+        {
+            clsApplication jobApp = new clsApplication();
+            String TestPosition = "Doctor";
+
+            jobApp.PoistionApplied = TestPosition;
+            Assert.AreEqual(jobApp.PoistionApplied, TestPosition);
         }
     }
 }
