3c86283e073553b6fbdbc8fb46d1d0ce8be2db51
Przemek Waliszka <P2729217@LEC-ADMIN.dmu.ac.uk>
2024-05-13 13:58:56 +0100

    create application class and unit tests

---
+++ ClassLibrary/clsApplication.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary
+{
+    public class clsApplication
+    {
+        // Fields
+        public int ApplicationId { get; private set; }
+        public int? AdminId { get; set; }
+        public int? StaffId { get; set; }
+        public string ApplicantName { get; private set; }
+        public string ContactNumber { get; private set; }
+        public string EmailAddress { get; private set; }
+        public string PoistionApplied { get; private set; }
+        public string Resume { get; private set; }
+
+        // Constructor
+
+        public clsApplication()
+        {
+        }
+        public clsApplication(string applicantName, string emailAddress, string positionApplied, string contactNumber, string resume = null)
+        {
+            ApplicantName = applicantName;
+            EmailAddress = emailAddress;
+            ContactNumber = contactNumber;
+            PoistionApplied = positionApplied;
+            Resume = resume;
+        }
+       
+    }
+}
---
+++ Testing4/tstApplication.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstApplication
+    {
+        [TestMethod]
+        public void instanceConstructorOk()
+        {
+            clsApplication jobApplication = new clsApplication();
+            Assert.IsNotNull(jobApplication);
+        }
+    }
+}
