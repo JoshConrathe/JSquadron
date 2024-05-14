054e34ad7dc95b48936c46029d51dff267e30b3d
Przemyslaw <115092435+prz3m01299@users.noreply.github.com>
2024-05-14 17:21:42 +0100

    Job application feature (#2)
    * initial commit for job application branch
    
    * add job app page
    
    * Add applicatyion form
    
    * create application class and unit tests
    
    * Add class and further unit tests
    
    * Presentation layer, viewer and data entry
    
    * Add class initialisation and session
    
    * modify clsApplication, add unit tests
    
    * Add find method, unit tests and button for it
    
    ---------
    
    Co-authored-by: Przemek Waliszka <P2729217@LEC-ADMIN.dmu.ac.uk>

--- AdminSystem/ApplicationDataEntry.aspx.cs
+++ AdminSystem/ApplicationDataEntry.aspx.cs
@@ -4,11 +4,77 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void SubmitButtonId_Click(object sender, EventArgs e)
+    {
+        clsApplication Application = new clsApplication();
+
+        int staffId = 0;
+        int.TryParse(Request.Form["txtStaffId"], out staffId);
+        string fullName = String.Format("{0}", Request.Form["txtFullName"]);
+        string contactNumber = String.Format("{0}", Request.Form["txtPhone"]);
+        string positionApplied = String.Format("{0}", Request.Form["txtPosition"]);
+        string emailAddress = String.Format("{0}", Request.Form["txtEmail"]);
+        string resume = String.Format("{0}", Request.Form["txtResume"]);
+
+        if (staffId != 0)
+        {
+            Application.StaffId = staffId;
+        }
+
+        Application.ApplicantName = fullName;
+        Application.ContactNumber = contactNumber;
+        Application.PositionApplied = positionApplied;
+        Application.EmailAddress = emailAddress;
+        if (resume != null)
+        {
+            Application.Resume = resume;
+        }
+        else
+        {
+            Application.Resume = null;
+        }
+
+        Session["Application"] = Application;
+        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Application submittion was successful!')", true);
+        Response.Redirect("ApplicationViewer.aspx");
+    }
+
+    protected void CancelButtonId_Click(object sender, EventArgs e)
+    {
+        Console.WriteLine("Cancel button clicked");
+        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Application cancelled!')", true);
+        Response.Redirect("ApplicationViewer.aspx");
+    }
+
+    protected void FindButton_Click(object sender, EventArgs e)
+    {
+        clsApplication jobApp = new clsApplication();
+        Int32 StaffId;
+        Boolean Found = false;
+        StaffId = Convert.ToInt32(txtStaffId.Value);
+
+        Found = jobApp.Find(StaffId);
+
+        if (Found == true)
+        {
+            txtFullName.Value = jobApp.ApplicantName;
+            txtEmail.Value = jobApp.EmailAddress;
+            txtPhone.Value = jobApp.ContactNumber;
+            txtPosition.Value = jobApp.PositionApplied;
+            if (jobApp.Resume != null)
+            {
+                txtResume.Value = jobApp.Resume;
+            }
+
+        }
     }
 }
