6bdf29b208fb64e4f83b489683ca60813dfb406c
SehmusCan6 <160249023+SehmusCan6@users.noreply.github.com>
2024-05-21 18:35:27 +0100

    Add files via upload

--- ClassLibrary/clsStaffCollection.cs
+++ ClassLibrary/clsStaffCollection.cs
@@ -6,6 +6,7 @@ namespace ClassLibrary
     public class clsStaffCollection
     {
         List<clsStaff> mStaffList = new List<clsStaff>();
+        clsStaff mThisStaff = new clsStaff();
         public List<clsStaff> StaffLIST
         {
             get
@@ -29,20 +30,39 @@ namespace ClassLibrary
             }
         }
 
-        public clsStaff ThisStaff { get; set; }
+        public clsStaff ThisStaff 
+        {
+            get 
+            {
+                return mThisStaff;
+            } set 
+            {
+                mThisStaff = value;
+            } 
+        }
+
+
 
         public clsStaffCollection()
         {
 
 
-            Int32 Index = 0;
-            Int32 RecordCount = 0;
+            
             clsDataConnection DB = new clsDataConnection();
             DB.Execute("staff_selectAll");
+            PopulateArray(DB);
+
+
+        }
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 RecordCount;
             RecordCount = DB.Count;
+            mStaffList =new List<clsStaff>();
             while (Index < RecordCount)
             {
-                clsStaff ANStaff= new clsStaff();
+                clsStaff ANStaff = new clsStaff();
                 ANStaff.StaffId = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffId"]);
                 ANStaff.StaffName = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
                 ANStaff.StaffPosition = Convert.ToString(DB.DataTable.Rows[Index]["Position"]);
@@ -53,8 +73,46 @@ namespace ClassLibrary
                 Index++;
 
             }
+        }
 
+        public int Add()
+        {
+           clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@StaffName", mThisStaff.StaffName);
+            DB.AddParameter("@StaffPosition", mThisStaff.StaffPosition);
+            DB.AddParameter("@ContactNumber", mThisStaff.StaffContactNumber);
+            DB.AddParameter("@Salary", mThisStaff.StaffSalary);
+            DB.AddParameter("@AdminId", mThisStaff.AdminID);
+            return DB.Execute("sproc_Staff_Insert");
+            
 
+        }
+
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("StaffId", mThisStaff.StaffId);
+            DB.AddParameter("@StaffName", mThisStaff.StaffName);
+            DB.AddParameter("@StaffPosition", mThisStaff.StaffPosition);
+            DB.AddParameter("@ContactNumber", mThisStaff.StaffContactNumber);
+            DB.AddParameter("@StaffSalary", mThisStaff.StaffSalary);
+            DB.AddParameter("@AdminId", mThisStaff.AdminID);
+            DB.Execute("sproc_Staff_Update");
+        }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@StaffId", mThisStaff.StaffId);
+            DB.Execute("sproc_Staff_Delete");
+        }
+
+        public void ReportByPosition(string Name)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@StaffName", Name);
+            DB.Execute("sproc_Staff_FilterByName");
+            PopulateArray(DB);
         }
     }
 }
