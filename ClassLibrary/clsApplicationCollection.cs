e9e41693d1210ca1f57e9fd86d24b359b282212d
Przemek Waliszka <P2729217@LEC-ADMIN.dmu.ac.uk>
2024-05-28 13:19:47 +0100

    class collection

---
+++ ClassLibrary/clsApplicationCollection.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsApplicationCollection
+    {
+        List<clsApplication> mApplicationList = new List<clsApplication>();
+        public List<clsApplication> ApplicationList 
+        {
+            get
+            {
+                return mApplicationList;
+            }
+            set
+            {
+                mApplicationList = value;
+            }
+        }
+        public int Count
+        { 
+            get
+            {
+                return mApplicationList.Count;
+            }
+                
+            set
+            {
+                
+            }
+        }
+        public clsApplication ThisApplication { get; set; }
+
+        /*public clsApplicationCollection()
+        {
+            clsApplication TestItem = new clsApplication();
+
+            TestItem.StaffId = 1;
+            TestItem.ApplicantName = "Test";
+            TestItem.EmailAddress = "Test";
+            TestItem.ContactNumber = "Test";
+            TestItem.PositionApplied = "Test";
+
+            mApplicationList.Add(TestItem);
+            TestItem = new clsApplication(); 
+
+            TestItem.StaffId = 1;
+            TestItem.ApplicantName= "Test";
+            TestItem.ContactNumber= "Test";
+            TestItem.EmailAddress= "Test";
+            TestItem.PositionApplied= "Test";
+            mApplicationList.Add(TestItem);
+        }*/
+
+        public clsApplicationCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("dbp.jobApplication_selectAll");
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsApplication AnApplication = new clsApplication();
+                AnApplication.StaffId = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffId"]);
+                AnApplication.ApplicantName = Convert.ToString(DB.DataTable.Rows[Index]["ApplicantName"]);
+                AnApplication.ContactNumber = Convert.ToString(DB.DataTable.Rows[Index]["ContactNumber"]);
+                AnApplication.EmailAddress = Convert.ToString(DB.DataTable.Rows[Index]["EmailAddress"]);
+                AnApplication.PositionApplied = Convert.ToString(DB.DataTable.Rows[Index]["PositionApplied"]);
+
+                mApplicationList.Add(AnApplication);
+                Index++;
+            }
+        }
+
+    }
+}
---
+++ Testing4/tstApplicationCollection.cs
@@ -0,0 +1,71 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstApplicationCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsApplicationCollection AllApplications = new clsApplicationCollection();
+            Assert.IsNotNull(AllApplications);
+
+
+            List<clsApplication> TestList = new List<clsApplication>();
+
+            clsApplication TestItem = new clsApplication();
+            TestItem.StaffId = 1;
+            TestItem.ApplicantName = "Test";
+            TestItem.ContactNumber = "Test";
+            TestItem.EmailAddress = "Test";
+            TestItem.PositionApplied = "Test";
+            TestItem.Resume = null;
+            
+            TestList.Add(TestItem);
+            AllApplications.ApplicationList = TestList;
+
+            Assert.AreEqual(AllApplications.ApplicationList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisApplicationPropertyOK()
+        {
+            clsApplicationCollection AllApplications = new clsApplicationCollection();
+            clsApplication TestApplication = new clsApplication();
+
+            TestApplication.StaffId = 1;
+            TestApplication.ApplicantName = "Test";
+            TestApplication.ContactNumber = "Test";
+            TestApplication.EmailAddress = "Test";
+            TestApplication.PositionApplied = "Test";
+
+            AllApplications.ThisApplication = TestApplication;
+            Assert.AreEqual(AllApplications.ThisApplication, TestApplication);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsApplicationCollection AllApp = new clsApplicationCollection();
+
+            List<clsApplication> TestList = new List<clsApplication>();
+            clsApplication TestItem = new clsApplication();
+
+            TestItem.StaffId = 1;
+            TestItem.ApplicantName = "Test";
+            TestItem.ContactNumber = "Test";
+            TestItem.EmailAddress = "Test";
+            TestItem.PositionApplied = "Test";
+            
+            TestList.Add(TestItem);
+            AllApp.ApplicationList = TestList;
+            Assert.AreEqual(AllApp.Count, TestList.Count);
+        }
+
+    }
+}
