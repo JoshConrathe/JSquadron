26dc0343aa40f4314dd0c5b619a941fc4f5d6bdb
SehmusCan6 <P2786563@my365.dmu.ac.uk>
2024-05-30 11:25:11 +0100

    Staff And Patient All functions works for example add, delete ,finder , find and validation. Also I did risk register and ethics checklist for staff and patient management.

--- AdminSystem/PatientConfirmDelete.aspx.cs
+++ AdminSystem/PatientConfirmDelete.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 PatientId;
     protected void Page_Load(object sender, EventArgs e)
     {
+       
+        
+            PatientId = Convert.ToInt32(Session["PatientID"]);
+        
+       
+    }
 
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        clsPatientCollection patientdelete = new clsPatientCollection();
+        patientdelete.ThisPatient.Find(PatientId);
+        patientdelete.Delete();
+        Response.Redirect("PatientList.aspx");
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("PatientList.aspx");
     }
 }
--- AdminSystem/PatientList.aspx.cs
+++ AdminSystem/PatientList.aspx.cs
@@ -7,16 +7,20 @@ using System.Web.UI.WebControls;
 using ClassLibrary;
 public partial class _1_List : System.Web.UI.Page
 {
+    
     protected void Page_Load(object sender, EventArgs e)
     {
       if(IsPostBack == false)
         {
-            DisplayPatients();
+      
+                DisplayPatientslist();
+          
+
         }
 
          
     }
-    void DisplayPatients()
+    void DisplayPatientslist()
     {
         clsPatientCollection patient = new clsPatientCollection();
         LstPatientList.DataSource = patient.PatientList;
@@ -25,5 +29,85 @@ public partial class _1_List : System.Web.UI.Page
         LstPatientList.DataBind();  
 
     }
- 
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session["PatientID"] = -1;
+        Response.Redirect("PatientDataEntry.aspx");
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Int32 PatientId;
+        if (LstPatientList.SelectedIndex != -1)
+        {
+            PatientId = Convert.ToInt32(LstPatientList.SelectedValue);
+            Session["PatientID"] = PatientId;
+            Response.Redirect("PatientDataEntry.aspx");
+        }
+        else 
+        {
+            lblEror.Text = "Please select a record from the list to edit";
+        }
+                
+                
+                
+                
+                
+                
+    }
+
+
+    protected void Unnamed1_Click1(object sender, EventArgs e)
+    {
+        Int32 PatientId;
+        if (LstPatientList.SelectedIndex != -1)
+        {
+            PatientId = Convert.ToInt32(LstPatientList.SelectedValue);
+            Session["PatientID"] = PatientId;
+            Response.Redirect("PatientConfirmDelete.aspx");
+
+        }
+        else
+        {
+            lblEror.Text = "Please select a record from the list to delete";
+        }
+
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        clsPatientCollection anpatient = new clsPatientCollection();
+        anpatient.ReportByAddress(txtPostCodeFilter.Text);
+        LstPatientList.DataSource = anpatient.PatientList;
+        LstPatientList.DataValueField = "PatientID";
+        LstPatientList.DataTextField = "PatientAddress";
+        LstPatientList.DataBind();
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        clsPatientCollection anpatient = new clsPatientCollection();
+        anpatient.ReportByAddress("");
+        txtPostCodeFilter.Text = "";
+        LstPatientList.DataSource= anpatient.PatientList;
+        LstPatientList.DataValueField = "PatientID";
+       
+        LstPatientList.DataTextField = "PatientAddress";
+
+        LstPatientList.DataBind();
+
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("TeamMainMenu.aspx");
+
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("PatientStatistics.aspx");
+    }
 }
