879f2a54b979584a0b05bebeb280f3ac9ff3b6a7
SehmusCan6 <P2786563@my365.dmu.ac.uk>
2024-05-30 09:20:51 +0100

    Add skeleton

--- AdminSystem/StaffList.aspx
+++ AdminSystem/StaffList.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="LstStaffList" runat="server" Height="380px" Width="265px"></asp:ListBox>
     </form>
 </body>
 </html>
--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,19 @@ public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayStaffLIST();
+        }
 
+
+    }
+    void DisplayStaffLIST()
+    {
+        clsStaffCollection STAFF = new clsStaffCollection();
+        LstStaffList.DataSource = STAFF.StaffLIST;
+        LstStaffList.DataValueField = "StaffId";
+        LstStaffList.DataTextField = "StaffName";
+        LstStaffList.DataBind();
     }
 }
